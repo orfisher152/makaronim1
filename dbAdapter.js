0ddd8d87ac18e6e07cbfec61291c878c22a43c13
Or Fisher <orfisher@gmail.com>
2022-05-30 12:30:27 +0300

    feat(mongoDB): add insert func

--- dbAdapter.js
+++ dbAdapter.js
@@ -1,3 +1,15 @@
-///text 1
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-///text 2 
+const insertDB =  function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("makronim1");
+    var myobj = { name: "green", price: "40" };
+    dbo.collection("product").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  }
+
+MongoClient.connect(url, insertDB);
